d947eec95f003514ee0b0f2b0239833d4dfbe995
lokesh18541 <155994069+lokesh18541@users.noreply.github.com>
2024-08-16 12:14:55 -0500

    Create marksavg.py

---
+++ marksavg.py
@@ -0,0 +1,8 @@
+print("enter marks of 3 subjects")
+maths=int(input("enter marks of maths"))
+physics=int(input("enter marks of phy"))
+che=int(input("enter marks of che"))
+if maths>= 45 and physics >= 45 and che >= 45:
+    print("pass")
+else:
+    print("fail")
