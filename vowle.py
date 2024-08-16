fca18b8f6f59d20781098e8337a4279527a958cc
lokesh18541 <155994069+lokesh18541@users.noreply.github.com>
2024-08-16 12:33:57 -0500

    vowle.py

---
+++ vowle.py
@@ -0,0 +1,5 @@
+literal=str(input("enter a lower case character"))
+if literal =='a'or literal =='e' or literal =='i'or literal =='o'or literal =='u':
+    print("vowel")
+else:
+    print("consonent")
